b9889c1606aed61907881c8e8af56649e3a68beb
dklbreitling <davidbreitling@web.de>
2021-02-14 16:35:08 +0100

    added documentation to mv*.sh mv_rest_to_Other.sh now applies to current dir if no arg is passed

--- mv_pdfs_to_PDFs.sh
+++ mv_pdfs_to_PDFs.sh
@@ -1,9 +1,11 @@
+# As the name suggests, moves all files matching '*.pdf' into directory 'PDFs'.
+
 #clear;
 original_dir=$(pwd);
 if [ -z "$1" ]; then
-	echo "Please pass as an argument the folder you want to apply the script to (with trailing '/').";
+	echo "Please pass as an argument the directory you want to apply the script to (with trailing '/').";
 else
-	echo "Moving all PDF's in '$1' into folder '$1Other'.";
+	echo "Moving all PDF's in '$1' into directory '$1PDFs'.";
 	cd "$1";
 	if [ ! -d PDFs ]; then
 		mkdir PDFs;
--- mv_rest_to_Other.sh
+++ mv_rest_to_Other.sh
@@ -1,13 +1,16 @@
+# Moves all files with a file ending (i.e. matching '*.*') into directory 'Other'.
+# Original use case was to move all files not previously moved by mv_pdfs_to_PDFs.sh.
+
 #clear;
 original_dir=$(pwd);
 if [ -z "$1" ]; then
-	echo "Please pass as an argument the folder you want to apply the script to (with trailing '/').";
-else
-	echo "Moving all files left in '$1' into folder '$1Other'.";
-	cd "$1";
-	if [ ! -d Other ]; then
-		mkdir Other;
-	fi
-	mv *.* Other/;
-	cd "$original_dir";
+	echo "No argument passed, applying script to current directory.";
+	set -- "$original_dir/"
 fi
+echo "Moving all files with file ending in '$1' into directory '$1Other'.";
+cd "$1";
+if [ ! -d Other ]; then
+	mkdir Other;
+fi
+mv *.* Other/;
+cd "$original_dir";
