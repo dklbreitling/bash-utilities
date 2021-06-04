e9b2da17950287918e1caa63ab2fa119382bdd4f
David Breitling <david.breitling@sgalinski.de>
2021-06-05 00:09:46 +0200

    added enum_files.sh

---
+++ enum_files.sh
@@ -0,0 +1,16 @@
+# $1 = wildcard
+# $2 = new name, e.g. $2=doc -> doc001, doc002, ...
+# $3 = file extension
+
+if [ "$#" -ne 3 ]; then
+	printf "Supply three arguments, \$1 = wildcard, \$2 = new name, \$3 = file extension."
+else
+	c=1
+
+	for file in $1
+	do
+		name=$(printf "$2%03d.$3" "$c")
+		mv -- "$file" "$name"
+		(( c++ ))
+fi
+
