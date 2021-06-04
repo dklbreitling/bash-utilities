d12b4e58839d9a81618b670c28bcf19e66eaab63
David Breitling <david.breitling@sgalinski.de>
2021-06-05 00:26:50 +0200

    fixed logic errors in enum_files.sh

--- enum_files.sh
+++ enum_files.sh
@@ -3,14 +3,15 @@
 # $3 = file extension
 
 if [ "$#" -ne 3 ]; then
-	printf "Supply three arguments, \$1 = wildcard, \$2 = new name, \$3 = file extension."
+	printf "Supply three arguments, \$1 = wildcard, \$2 = new name, \$3 = file extension.\n"
 else
 	c=1
-
+	
 	for file in $1
 	do
 		name=$(printf "$2%03d.$3" "$c")
 		mv -- "$file" "$name"
 		(( c++ ))
+	done
 fi
 
